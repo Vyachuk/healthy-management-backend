228ca346231b0ac916a37913481b23d35cf3466d
Vyachuk <Slavkoka045@gmail.com>
2023-12-15 01:03:26 +0200

    Edit: change Olga Photo

--- helpers/elasticEmailSend.js
+++ helpers/elasticEmailSend.js
@@ -24,7 +24,7 @@ const sendDataToEmail = ({ name, email, phone, comment, service }) => {
             <p style="margin-bottom: 37px; margin-top: 0;">Доброго дня, ${name}! <br/><br/>Дякуємо, що звернулися до студії Health Management. Мене звати Ольга Поліщук, я власниця та засновниця студії, я дякую вам за довіру та запит. Ми раді допомогти вам стати кращими. Зв'яжемося з вами протягом 24 годин.<br/><br/>З повагою, </p>
 
             <div style="width: 360px;background-color: rgb(6, 33, 54); border-radius: 16px;  padding: 24px; display: flex; margin-bottom: 37px"> 
-              <img style="display: inline-block; border-radius: 16px; width: 112px; height: 112px; margin-right: 24px" src='https://healthy-management.onrender.com/olgaPhoto.png' alt="Olga Polichshyk">
+              <img style="display: inline-block; border-radius: 16px; width: 112px; height: 112px; margin-right: 24px" src='https://healthy-management.onrender.com/olga.png' alt="Olga Polichshyk">
               <div >
                 <h2 style="margin-top: 0; color: white; font-size: 16px; font-weight: 600; line-height: 24px; margin-bottom: 8px" >Ольга Поліщук</h2>
                 <p style="color: white; font-size: 12px; line-height: 16px; margin-top: 0; margin-bottom: 8px" >+380 673 160 556</p>
