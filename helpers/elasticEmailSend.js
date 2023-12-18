f9385fd912cfd0e5cad56bac3fde8587754121cc
Vyachuk <Slavkoka045@gmail.com>
2023-12-18 23:50:20 +0200

    EDIT mail list

--- helpers/elasticEmailSend.js
+++ helpers/elasticEmailSend.js
@@ -19,20 +19,20 @@ const sendDataToEmail = ({ name, email, phone, comment, service }) => {
         ElasticEmail.BodyPart.constructFromObject({
           ContentType: "HTML",
           Content: `
-          <div style="max-width: 688px; margin: 0 auto; color: black; font-family: 'Montserrat', sans-serif;">
+          <div style="max-width: 688px; color: black; margin: 0 auto; color: black; font-family: 'Montserrat', sans-serif;">
             <a href="healthymanagement.com.ua"><img style="width: 227px; margin-bottom: 37px" src='https://healthy-management.onrender.com/newlogo.png' alt="Healthy Management logo"></a>
             <p style="margin-bottom: 37px; margin-top: 0;">Доброго дня, ${name}! <br/><br/>Дякуємо, що звернулися до студії Healthy Management. Мене звати Ольга Поліщук, я власниця та засновниця студії, дякую вам за довіру та запит. Ми раді допомогти вам стати кращими. Зв'яжемося з вами протягом 24 годин.<br/><br/>З повагою, </p>
 
             <div style="width: 360px; border-radius: 16px;  padding: 24px; display: flex; margin-bottom: 37px"> 
               <img style="display: inline-block; border-radius: 16px; width: 112px; height: 112px; margin-right: 24px" src='https://healthy-management.onrender.com/olga.png' alt="Olga Polichshyk">
               <div >
-                <h2 style="margin-top: 0; color: white; font-size: 16px; font-weight: 600; line-height: 24px; margin-bottom: 8px" >Ольга Поліщук</h2>
-                <p style="color: white; font-size: 12px; line-height: 16px; margin-top: 0; margin-bottom: 8px" >+380 673 160 556</p>
-                <p style="color: white; text-decoration: none; font-size: 12px; line-height: 16px; margin-top: 0;  margin-bottom: 16px" >healthymanagement.com.ua</p>
+                <h2 style=" margin-top: 0; font-size: 16px; font-weight: 600; line-height: 24px; margin-bottom: 8px" >Ольга Поліщук</h2>
+                <p style=" font-size: 12px; line-height: 16px; margin-top: 0; margin-bottom: 8px" >+380 673 160 556</p>
+                <p style=" text-decoration: none; font-size: 12px; line-height: 16px; margin-top: 0;  margin-bottom: 16px" >healthymanagement.com.ua</p>
                 <div style="margin-top: 20px">
-                  <a style="margin-right: 16px" target="_blank" href="https://www.instagram.com/olga_healthm/"><img style="width: 16px; height: 16px" src="https://healthy-management.onrender.com/instagram.png" alt="Instagram link" /></a>
+                  <a style="margin-right: 16px" target="_blank" href="https://www.instagram.com/olga_healthm/"><img style="width: 24px; height: 24px" src="https://healthy-management.onrender.com/instagram.png" alt="Instagram link" /></a>
                   <a style="margin-right: 16px" target="_blank" href="https://www.facebook.com/olga.polishchuk.14"><img style="width: 16px; height: 16px" src="https://healthy-management.onrender.com/facebook.png" alt="Facebook link" /></a>
-                  <a target="_blank" href="https://www.linkedin.com/in/olga-polishchuk-83362140/"><img style="width: 16px; height: 16px" src="https://healthy-management.onrender.com/linkedin.png" alt="Linkedin link" /></a>
+                  <a target="_blank" href="https://www.linkedin.com/in/olga-polishchuk-83362140/"><img style="width: 24px; height: 24px" src="https://healthy-management.onrender.com/linkedin.png" alt="Linkedin link" /></a>
               </div>
             </div>
           </div>
